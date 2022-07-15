3bad8d473c4429d14f0705f6bc99fe804c11e263
Youssef <YoussefAhab@outlook.com>
2022-07-15 22:56:59 +0200

    Initial Commit

---
+++ VINA-Frontend/VINA_FrontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  VINA_FrontendApp.swift
+//  VINA-Frontend
+//
+//  Created by Youssef Ahab on 15/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct VINA_FrontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
